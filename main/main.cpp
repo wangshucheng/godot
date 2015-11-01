wangshucheng/godot

commit bcee5b5be04f59c3c461680f3f641fa01391f13a

ran cppcheck, fixed cases where array index is used before limits check

main/main.cpp
@@ -1006,61 +1006,49 @@ bool Main::start() {
 	bool export_debug=false;
 	List<String> args = OS::get_singleton()->get_cmdline_args();
 	for (int i=0;i<args.size();i++) {
-		
-
-		if (args[i]=="-doctool" && i <(args.size()-1)) {
-
-			doc_tool=args[i+1];
+		//parameters that have an argument to the right
+		if (i < (args.size()-1)) {
+			if (args[i]=="-doctool") {
+				doc_tool=args[i+1];
+			} else if (args[i]=="-script" || args[i]=="-s") {
+				script=args[i+1];
+			} else if (args[i]=="-level" || args[i]=="-l") {
+				OS::get_singleton()->_custom_level=args[i+1];
+			} else if (args[i]=="-test") {
+				test=args[i+1];
+			} else if (args[i]=="-optimize") {
+				optimize=args[i+1];
+			} else if (args[i]=="-optimize_preset") {
+				optimize_preset=args[i+1];
+			} else if (args[i]=="-export") {
+				editor=true; //needs editor
+				_export_platform=args[i+1];
+			} else if (args[i]=="-export_debug") {
+				editor=true; //needs editor
+				_export_platform=args[i+1];
+				export_debug=true;
+			} else if (args[i]=="-import") {
+				editor=true; //needs editor
+				_import=args[i+1];
+			} else if (args[i]=="-import_script") {
+				editor=true; //needs editor
+				_import_script=args[i+1];
+			} else if (args[i]=="-dumpstrings") {
+				editor=true; //needs editor
+				dumpstrings=args[i+1];
+			}
 			i++;
-		}else if (args[i]=="-nodocbase") {
-
+		}
+		//parameters that do not have an argument to the right
+		if (args[i]=="-nodocbase") {
 			doc_base=false;
-		} else if ((args[i]=="-script" || args[i]=="-s") && i <(args.size()-1)) {
-		
-			script=args[i+1];
-			i++;
-		} else if ((args[i]=="-level" || args[i]=="-l") && i <(args.size()-1)) {
-
-			OS::get_singleton()->_custom_level=args[i+1];
-			i++;
-		} else if (args[i]=="-test" && i <(args.size()-1)) {
-			test=args[i+1];
-			i++;
-		} else if (args[i]=="-optimize" && i <(args.size()-1)) {
-			optimize=args[i+1];
-			i++;
-		} else if (args[i]=="-optimize_preset" && i <(args.size()-1)) {
-			optimize_preset=args[i+1];
-			i++;
-		} else if (args[i]=="-export" && i <(args.size()-1)) {
-			editor=true; //needs editor
-			_export_platform=args[i+1];
-			i++;
-		} else if (args[i]=="-export_debug" && i <(args.size()-1)) {
-			editor=true; //needs editor
-			_export_platform=args[i+1];
-			export_debug=true;
-			i++;
-		} else if (args[i]=="-import" && i <(args.size()-1)) {
-			editor=true; //needs editor
-			_import=args[i+1];
-			i++;
-		} else if (args[i]=="-import_script" && i <(args.size()-1)) {
-			editor=true; //needs editor
-			_import_script=args[i+1];
-			i++;
-		} else if (args[i]=="-noquit" ) {
+		} else if (args[i]=="-noquit") {
 			noquit=true;
-		} else if (args[i]=="-dumpstrings" && i <(args.size()-1)) {
-			editor=true; //needs editor
-			dumpstrings=args[i+1];
-			i++;
-		} else if (args[i]=="-editor" || args[i]=="-e") {
-			editor=true;
 		} else if (args[i]=="-convert_old") {
 			convert_old=true;
+		} else if (args[i]=="-editor" || args[i]=="-e") {
+			editor=true;
 		} else if (args[i].length() && args[i][0] != '-' && game_path == "") {
-
 			game_path=args[i];
 		}
 	}

scene/gui/text_edit.cpp
@@ -709,7 +709,7 @@ void TextEdit::_notification(int p_what) {
 						if (in_region==-1 && !in_keyword && is_char && !prev_is_char) {
 							
 							int to=j;
-							while(_is_text_char(str[to]) && to<str.length())
+							while(to<str.length() && _is_text_char(str[to]))
 								to++;
 							
 							uint32_t hash = String::hash(&str[j],to-j);

commit 666f3dd2ebbf6050241b88f9ada497b7f48c38b3

Fix arguments parsing in the main function
Fixes #2611 which was a regression from bcee5b5be04f59c3c461680f3f641fa01391f13a.
The bogus behaviour considered that if there were more than one arguments left to parse,
they would be a pair of arguments (switch and its parameter), and thus skipped the next
argument in all cases (thus potentially skipping a "-editor", which triggered #2611).

This is fixed by checking first for arguments that don't expect a parameter, and only
afterwards for arguments that expect a parameter. And if a "pair" of arguments is not
valid, we no longer increment the counter.

main/main.cpp
@@ -1007,8 +1007,21 @@ bool Main::start() {
 	bool export_debug=false;
 	List<String> args = OS::get_singleton()->get_cmdline_args();
 	for (int i=0;i<args.size();i++) {
+		//parameters that do not have an argument to the right
+		if (args[i]=="-nodocbase") {
+			doc_base=false;
+		} else if (args[i]=="-noquit") {
+			noquit=true;
+		} else if (args[i]=="-convert_old") {
+			convert_old=true;
+		} else if (args[i]=="-editor" || args[i]=="-e") {
+			editor=true;
+		} else if (args[i].length() && args[i][0] != '-' && game_path == "") {
+			game_path=args[i];
+		}
 		//parameters that have an argument to the right
-		if (i < (args.size()-1)) {
+		else if (i < (args.size()-1)) {
+			bool parsed_pair=true;
 			if (args[i]=="-doctool") {
 				doc_tool=args[i+1];
 			} else if (args[i]=="-script" || args[i]=="-s") {
@@ -1037,20 +1050,13 @@ bool Main::start() {
 			} else if (args[i]=="-dumpstrings") {
 				editor=true; //needs editor
 				dumpstrings=args[i+1];
+			} else {
+				// The parameter does not match anything known, don't skip the next argument
+				parsed_pair=false;
 			}
-			i++;
-		}
-		//parameters that do not have an argument to the right
-		if (args[i]=="-nodocbase") {
-			doc_base=false;
-		} else if (args[i]=="-noquit") {
-			noquit=true;
-		} else if (args[i]=="-convert_old") {
-			convert_old=true;
-		} else if (args[i]=="-editor" || args[i]=="-e") {
-			editor=true;
-		} else if (args[i].length() && args[i][0] != '-' && game_path == "") {
-			game_path=args[i];
+			if (parsed_pair) {
+				i++;
+			}
 		}
 	}
 
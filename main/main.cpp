wangshucheng/godot

commit 9cea5a3d56b54dbebf6619ae0ad50139dc91e769

improve reporting of error in wrong inheritance for autoload script

main/main.cpp
@@ -1374,12 +1374,13 @@ bool Main::start() {
 						} else if (res->is_type("Script")) {
 							Ref<Script> s = res;
 							StringName ibt = s->get_instance_base_type();
+							bool valid_type = !ObjectTypeDB::is_type(ibt,"Node");
 							ERR_EXPLAIN("Script does not inherit a Node: "+path);
-							ERR_CONTINUE( !ObjectTypeDB::is_type(ibt,"Node") );
+							ERR_CONTINUE( !valid_type );
 
 							Object *obj = ObjectTypeDB::instance(ibt);
 
-							ERR_EXPLAIN("Cannot instance node for autoload type: "+String(ibt));
+							ERR_EXPLAIN("Cannot instance script for autoload, expected 'Node' inheritance, got: "+String(ibt));
 							ERR_CONTINUE( obj==NULL );
 
 							n = obj->cast_to<Node>();

drivers/unix/os_unix.cpp
@@ -78,11 +78,11 @@ void OS_Unix::print_error(const char* p_function,const char* p_file,int p_line,c
 			break;
 		case ERR_WARNING:
 			print("\E[1;33mWARNING: %s: \E[0m\E[1m%s\n",p_function,err_details);
-			print("\E[0;33m     At: %s:%i.\E[0m\n",p_file,p_line);
+			print("\E[0;33m   At: %s:%i.\E[0m\n",p_file,p_line);
 			break;
 		case ERR_SCRIPT:
 			print("\E[1;35mSCRIPT ERROR: %s: \E[0m\E[1m%s\n",p_function,err_details);
-			print("\E[0;35m          At: %s:%i.\E[0m\n",p_file,p_line);
+			print("\E[0;35m   At: %s:%i.\E[0m\n",p_file,p_line);
 			break;
 	}
 }

commit ebc1888175f386465e2ce48abb04c5e9b7050c7a

Fix wrong type check for autoload
Regression from 9cea5a3. Fixes #3342

main/main.cpp
@@ -1374,7 +1374,7 @@ bool Main::start() {
 						} else if (res->is_type("Script")) {
 							Ref<Script> s = res;
 							StringName ibt = s->get_instance_base_type();
-							bool valid_type = !ObjectTypeDB::is_type(ibt,"Node");
+							bool valid_type = ObjectTypeDB::is_type(ibt,"Node");
 							ERR_EXPLAIN("Script does not inherit a Node: "+path);
 							ERR_CONTINUE( !valid_type );
 
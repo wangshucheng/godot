wangshucheng/godot

commit 6ff1db1f5dc5c8d052a697a5cd8776bcfc12a28f

Fix various valgrind reported uninitialized variable uses

modules/gdnative/nativescript/nativescript.cpp
@@ -957,6 +957,7 @@ void NativeScriptLanguage::_unload_stuff(bool p_reload) {
 
 NativeScriptLanguage::NativeScriptLanguage() {
 	NativeScriptLanguage::singleton = this;
+	has_objects_to_register = false;
 #ifndef NO_THREADS
 	mutex = Mutex::create();
 #endif

core/command_queue_mt.h
@@ -309,9 +309,9 @@ class CommandQueueMT {
 	};
 
 	uint8_t command_mem[COMMAND_MEM_SIZE];
-	uint32_t read_ptr;
-	uint32_t write_ptr;
-	uint32_t dealloc_ptr;
+	uint32_t read_ptr = 0;
+	uint32_t write_ptr = 0;
+	uint32_t dealloc_ptr = 0;
 	SyncSemaphore sync_sems[SYNC_SEMAPHORES];
 	Mutex *mutex;
 	Semaphore *sync;

editor/editor_audio_buses.cpp
@@ -707,6 +707,9 @@ EditorAudioBus::EditorAudioBus(EditorAudioBuses *p_buses, bool p_is_master) {
 		channel[i].vu_r->set_min(-80);
 		channel[i].vu_r->set_max(24);
 		channel[i].vu_r->set_step(0.1);
+
+		channel[i].peak_l = 0.0f;
+		channel[i].peak_r = 0.0f;
 	}
 
 	scale = memnew(TextureRect);

editor/plugins/texture_region_editor_plugin.cpp
@@ -783,6 +783,7 @@ TextureRegionEditor::TextureRegionEditor(EditorNode *p_editor) {
 
 	snap_step = Vector2(10, 10);
 	snap_separation = Vector2(0, 0);
+	snap_mode = SNAP_NONE;
 	edited_margin = -1;
 	drag_index = -1;
 	drag = false;

platform/x11/os_x11.cpp
@@ -1193,6 +1193,7 @@ bool OS_X11::is_window_maximized() const {
 	unsigned long len;
 	unsigned long remaining;
 	unsigned char *data = NULL;
+	bool retval = false;
 
 	int result = XGetWindowProperty(
 			x11_display,
@@ -1221,13 +1222,15 @@ bool OS_X11::is_window_maximized() const {
 			if (atoms[i] == wm_max_vert)
 				found_wm_max_vert = true;
 
-			if (found_wm_max_horz && found_wm_max_vert)
-				return true;
+			if (found_wm_max_horz && found_wm_max_vert) {
+				retval = true;
+				break;
+			}
 		}
-		XFree(atoms);
 	}
 
-	return false;
+	XFree(data);
+	return retval;
 }
 
 void OS_X11::set_window_always_on_top(bool p_enabled) {

scene/gui/tabs.cpp
@@ -882,4 +882,6 @@ Tabs::Tabs() {
 
 	min_width = 0;
 	scrolling_enabled = true;
+	buttons_visible = false;
+	hover = -1;
 }

scene/gui/tree.cpp
@@ -3911,6 +3911,8 @@ Tree::Tree() {
 	cache.click_id = -1;
 	cache.click_item = NULL;
 	cache.click_column = 0;
+	cache.hover_cell = -1;
+	cache.hover_index = -1;
 	last_keypress = 0;
 	focus_in_id = 0;
 

scene/main/viewport.cpp
@@ -2789,6 +2789,7 @@ Viewport::Viewport() {
 	gui.drag_preview = NULL;
 	gui.drag_attempted = false;
 	gui.canvas_sort_index = 0;
+	gui.roots_order_dirty = false;
 
 	msaa = MSAA_DISABLED;
 	hdr = true;

commit cfde23a1018a2d8f9cf6dd722083de7e572e1e47

NativeScript: Fix initialization in wrong scope
Regression from 6ff1db1f5dc5c8d052a697a5cd8776bcfc12a28f which broke javascript build.

modules/gdnative/nativescript/nativescript.cpp
@@ -957,8 +957,8 @@ void NativeScriptLanguage::_unload_stuff(bool p_reload) {
 
 NativeScriptLanguage::NativeScriptLanguage() {
 	NativeScriptLanguage::singleton = this;
-	has_objects_to_register = false;
 #ifndef NO_THREADS
+	has_objects_to_register = false;
 	mutex = Mutex::create();
 #endif
 }
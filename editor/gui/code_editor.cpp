xianofantasia/Go

Fix for a scripts panel state not being saved when toggle button is used

editor/code_editor.cpp
@@ -1587,6 +1587,7 @@ void CodeTextEditor::_set_show_warnings_panel(bool p_show) {
 void CodeTextEditor::_toggle_scripts_pressed() {
 	ERR_FAIL_NULL(toggle_scripts_list);
 	toggle_scripts_list->set_visible(!toggle_scripts_list->is_visible());
+	EditorSettings::get_singleton()->set_project_metadata("scripts_panel", "show_scripts_panel", toggle_scripts_list->is_visible());
 	update_toggle_scripts_button();
 }
 

wangshucheng/godot

editor/gui/code_editor.cpp
@@ -1672,6 +1672,7 @@ void CodeTextEditor::_set_show_warnings_panel(bool p_show) {
 void CodeTextEditor::_toggle_files_pressed() {
 	ERR_FAIL_NULL(toggle_files_list);
 	toggle_files_list->set_visible(!toggle_files_list->is_visible());
+	EditorSettings::get_singleton()->set_project_metadata("files_panel", "show_files_panel", toggle_files_list->is_visible());
 	update_toggle_files_button();
 }
 